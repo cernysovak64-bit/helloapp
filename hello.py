3f037b6f493b1b0bac0c330302606333b5b0de7b
white <white@example.com>
2026-07-05 18:34:14 +0500

    Add greetWhite function

--- hello.py
+++ hello.py
@@ -4,3 +4,12 @@ def main():
 if __name__ == "__main__":
     main()
     
+def main():
+    print("hello")
+
+def greet_white():
+    print("hello white")
+
+if __name__ == "__main__":
+    main()
+    
